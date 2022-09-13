6f04a181186c5c8528c4a16d0dedbab3acd49454
emilio <emilio@mpbwalt.local>
2022-09-13 16:30:21 +0200

    minor

--- pyDriver/driver.py
+++ pyDriver/driver.py
@@ -78,8 +78,8 @@ if __name__ == '__main__':
     #query fligth
     queryData={"fromAirport": "FCO",
                 "toAirport": "LHR",
-                "fromDate": "Tue Sep 13 2022 00:00:00 GMT+0200 (Ora legale dell’Europa centrale)",
-                "returnDate": "Tue Sep 13 2022 00:00:00 GMT+0200 (Ora legale dell’Europa centrale)",
+                "fromDate": "Wed Sep 14 00:00:00 CEST 2022",
+                "returnDate": "Wed Sep 14 00:00:00 CEST 2022",
                 "oneWay": False}
     r = s.post(url="http://localhost/flight/queryflights",data=queryData)
     print(r.text)
