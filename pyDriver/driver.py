93e3c5ff9487275703a5ddf37628f06d669662a3
emilio <emilio@mpbwalt.local>
2022-09-13 16:35:53 +0200

    data

--- pyDriver/driver.py
+++ pyDriver/driver.py
@@ -82,7 +82,10 @@ if __name__ == '__main__':
                 "returnDate": "Tue Sep 13 2022 00:00:00 GMT+0200 (Ora legale dell’Europa centrale)",
                 "oneWay": False}
     r = s.post(url="http://localhost/flight/queryflights",data=queryData)
-    print(r.text)
+
+    flightData=json.loads(r.text);
+    print(flightData)
+    
     s.close()
     
     
