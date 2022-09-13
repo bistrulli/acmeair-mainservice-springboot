f2362d9b7c39773bec93cc5fdbc71f8723cb79e4
emilio <emilio@mpbwalt.local>
2022-09-13 08:13:41 +0200

    minor

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,26 +3,32 @@
 cd ../../
 
 cd acmeair-mainservice-springboot/
+git pull
 mvn clean package
 cd ../
 
 
 cd acmeair-authservice-springboot/
+git pull
 mvn clean package
 cd ../
 
 cd acmeair-customerservice-springboot/
+git pull
 mvn clean package
 cd ../
 
 cd acmeair-flightservice-springboot/
+git pull
 mvn clean package
 cd ../
 
 cd acmeair-bookingservice-springboot/
+git pull
 mvn clean package
 cd ../
 
 cd acmeair-mainservice-springboot/
+git pull
 mvn clean package
 cd ../
