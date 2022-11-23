cca8537a0daf57a24ea30c91fb6356a9f533236a
emilio-imt <emilio-imt@MacBook-Pro.homenet.telecomitalia.it>
2022-11-23 22:23:08 +0100

    refined simulated cpu

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,37 +4,37 @@ cd ../../
 
 cd acmeair-mainservice-springboot/
 git add -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
 
 
 cd acmeair-authservice-springboot/
 git add . -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
 
 cd acmeair-customerservice-springboot/
 git add . -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
 
 cd acmeair-flightservice-springboot/
 git add . -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
 
 cd acmeair-bookingservice-springboot/
 git add . -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
 
 cd acmeair-mainservice-springboot/
 git add . -A
-git commit -m $1
+git commit -m "$1"
 git push
 cd ../
