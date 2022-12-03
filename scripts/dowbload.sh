749af19544cdf3300dfbce327b351d39520708d3
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-12-03 11:13:08 +0100

    minor

---
+++ scripts/dowbload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+cd ~/git
+
+git clone --branch ctrl https://github.com/bistrulli/acmeair-authservice-springboot.git
+git clone --branch ctrl https://github.com/bistrulli/acmeair-bookingservice-springboot.git
+git clone --branch ctrl https://github.com/bistrulli/acmeair-customerservice-springboot.git
+git clone --branch ctrl https://github.com/bistrulli/acmeair-flightservice-springboot.git
+git clone https://github.com/bistrulli/acmeair-ctrlmnt-springboot.git
