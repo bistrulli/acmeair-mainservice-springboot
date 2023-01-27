0caa32fb733e3bfdc4902d3656cf2e3ddfac3380
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-27 12:50:54 +0100

    minor

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,6 @@ sudo apt install openjdk-17-jdk pip redis haproxy build-essential -y
 
 sudo /etc/init.d/haproxy stop
 sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/sbin/haproxy
+
+git config --global user.email 183048+bistrulli@users.noreply.github.com
+git config --global user.name  EMI
