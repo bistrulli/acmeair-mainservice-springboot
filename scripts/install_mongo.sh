be9524da3176e8c19fe45ca08e07cea86ce72084
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-27 10:14:56 +0100

    minor

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -6,5 +6,7 @@ sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-prop
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sudo add-apt-repository 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse'
 
+sudo apt update
+
  sudo apt install mongodb-org
  sudo systemctl enable --now mongod
