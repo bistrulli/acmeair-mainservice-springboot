a5495bbeb6acf8ceaf2bd9da9fe4329d56a96929
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-27 11:17:42 +0100

    minor

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-sudo apt update
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+sudo apt-get install gnupg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl status mongod
+sudo systemctl enable mongod
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-sudo add-apt-repository 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse'
 
-sudo apt update
-
- sudo apt install mongodb-org
- sudo systemctl enable --now mongod
